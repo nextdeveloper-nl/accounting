08ca906c365f67286039e401d5d924593a479e2d
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-12-20 16:36:07 +0300

    Invoice helper is updated, Accounting helper is updated

--- src/Helpers/AccountingHelper.php
+++ src/Helpers/AccountingHelper.php
@@ -3,11 +3,18 @@
 namespace Helpers;
 
 use NextDeveloper\Accounting\Database\Models\Accounts;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
+use NextDeveloper\IAM\Helpers\UserHelper;
 
 class AccountingHelper
 {
-    public static function getAccount() : Accounts
+    public static function getAccount(\NextDeveloper\IAM\Database\Models\Accounts $account = null) : Accounts
     {
+        if(!$account)
+            $account = UserHelper::currentAccount();
 
+        return Accounts::withoutGlobalScope(AuthorizationScope::class)
+            ->where('iam_account_id', $account->id)
+            ->first();
     }
 }
---
+++ src/Invoicing/AbstractInvoiceItem.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace NextDeveloper\Accounting\Invoicing;
+
+use Carbon\Carbon;
+use Helpers\AccountingHelper;
+use Helpers\InvoiceHelper;
+use NextDeveloper\Accounting\Database\Models\InvoiceItems;
+use NextDeveloper\Accounting\Services\InvoiceItemsService;
+use NextDeveloper\Commons\Database\Models\Currencies;
+use NextDeveloper\Events\Services\Events;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
+use NextDeveloper\IAM\Helpers\UserHelper;
+
+/**
+ *
+ */
+abstract class AbstractInvoiceItem
+{
+    protected $model;
+
+    private $invoice;
+
+    protected $from;
+
+    protected $to;
+
+    public function __construct($model, $year, $month)
+    {
+        $this->model = $model;
+
+        $this->from = Carbon::createFromDate($year, $month)->setTimezone('GMT')->startOfMonth();
+        $this->to = $this->from->copy()->endOfMonth();
+
+        $this->invoice = InvoiceHelper::getInvoice(
+            AccountingHelper::getAccount(
+                UserHelper::getAccountById( $this->model->iam_account_id )
+            ),
+            $year,
+            $month
+        );
+    }
+
+    protected function setItemCost($cost, Currencies $currency) {
+        $item = InvoiceItems::withoutGlobalScope(AuthorizationScope::class)
+            ->where('object_type', get_class($this->model))
+            ->where('object_id', $this->model->id)
+            ->where('accounting_invoice_id', $this->invoice->id)
+            ->first();
+
+        if(!$item) {
+            $item = InvoiceItemsService::create([
+                'accounting_invoice_id' =>  $this->invoice->id,
+                'object_type'   =>  is_object($this->model) ? get_class($this->model) : $this->model,
+                'object_id'     =>  is_object($this->model) ? $this->model->id : 0,
+                'quantity'      =>  1,
+                'unit_price'    =>  $cost,
+                'common_currency_id'    =>  $currency->id
+            ]);
+        }
+
+        $item->update([
+            'unit_price'    =>  $cost
+        ]);
+
+        Events::fire('updated:NextDeveloper\Accounting\InvoiceItems', $item);
+
+        InvoiceHelper::updateInvoiceAmount($this->invoice);
+
+        return $item;
+    }
+}
