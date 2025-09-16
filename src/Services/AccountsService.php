30bcda19e318fea387401a4c42b116473f2ea520
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-09-16 20:28:47 +0300

    Accounts disable and enable feature added

--- src/Services/AccountsService.php
+++ src/Services/AccountsService.php
@@ -2,8 +2,10 @@
 
 namespace NextDeveloper\Accounting\Services;
 
+use NextDeveloper\Accounting\Database\Models\Accounts;
 use NextDeveloper\Accounting\Database\Models\Partnerships;
 use NextDeveloper\Accounting\Services\AbstractServices\AbstractAccountsService;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
 
 /**
  * This class is responsible from managing the data for Accounts
@@ -16,4 +18,39 @@ class AccountsService extends AbstractAccountsService
 {
 
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+
+    public static function suspend(Accounts $account)
+    {
+        $account->update([
+            'is_suspended' =>  'true'
+        ]);
+
+        return $account->fresh();
+    }
+
+    public static function suspendWithIamAccount(\NextDeveloper\IAM\Database\Models\Accounts $account) {
+        $account = Accounts::withoutGlobalScope(AuthorizationScope::class)
+            ->where('iam_account_id', $account->id)
+            ->first();
+
+        return self::suspend($account);
+    }
+
+    public static function unsuspend(Accounts $account)
+    {
+        $account->update([
+            'is_suspended' =>  'false'
+        ]);
+
+        return $account->fresh();
+    }
+
+    public static function unsuspendWithIamAccount(\NextDeveloper\IAM\Database\Models\Accounts $account)
+    {
+        $account = Accounts::withoutGlobalScope(AuthorizationScope::class)
+            ->where('iam_account_id', $account->id)
+            ->first();
+
+        return self::unsuspend($account);
+    }
 }
